90de368daf0c9417ca456e904e2ad4f3f0375ba4
Nnamdi <koodu-platform@protonmail.com>
2024-04-16 16:24:31 +0100

    a

--- poc.dynamo.dev/client.js
+++ poc.dynamo.dev/client.js
@@ -1,13 +1,12 @@
-window.onload = function() {
-    const projectPath = '/path/to/your/frontend/project';
+window.addEventListener('load', function() {
+    const projectPath = 'src/';
     const entryPoint = 'src/main.js'; // Change this to your entry point file
     const bundleEndpoint = `http://localhost:3000/bundle?projectPath=${encodeURIComponent(projectPath)}&entryPoint=${encodeURIComponent(entryPoint)}`;
 
     fetch(bundleEndpoint)
         .then(response => response.text())
         .then(bundle => {
-            // Execute bundled code
             eval(bundle);
         })
         .catch(error => console.error('Error fetching bundle:', error));
-};
+});
