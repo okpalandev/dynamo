0e216face62e9c8f4e30c8603ed625d6cb3f5f1d
Nnamdi <koodu-platform@protonmail.com>
2024-04-16 15:44:54 +0100

    a

--- dynamo/src/index.ts
+++ dynamo/src/index.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'dynamo';
+import {CommandRunner,createCommand} from "dynamo/cli";
 
 export default defineConfig(function ({ command }) {
   const runner = new CommandRunner(createCommand);
